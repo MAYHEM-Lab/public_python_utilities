d5f6e1224939f974f3f329ee9361ee1c8cf34afb
ckrintz <ckrintz@gmail.com>
2022-12-10 13:06:29 -0800

    clean up

--- raspi-dht/instructables.py
+++ raspi-dht/instructables.py
@@ -1,11 +1,12 @@
+#from: https://www.instructables.com/Raspberry-Pi-Tutorial-How-to-Use-the-DHT-22/
 #Libraries
 import Adafruit_DHT as dht
 from time import sleep
 #Set DATA pin
-DHT = 4
+DHT = 4 #change this if you use a different GPIO pin
 while True:
-    #Read Temp and Hum from DHT22
-    h,t = dht.read_retry(dht.DHT22, DHT)
+    #Read Temp and Hum from DHT22 or DHT11 (change the args below depending)
+    h,t = dht.read_retry(dht.DHT11, DHT)
     #Print Temperature and Humidity on Shell window
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
     sleep(5) #Wait 5 seconds and read again
